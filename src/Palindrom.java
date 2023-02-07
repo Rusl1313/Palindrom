33b182fc4d8b63803cbeed9dba9b743ebe6e21b4
rusik <rusikss.ss13@gmail.com>
2023-02-07 22:10:19 +0300

    Palindom

---
+++ src/Palindrom.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Palindrom {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String symbol = scan.nextLine();
+        String s = symbol.replaceAll("[^a-zA-Z0-9]", "");
+        char[] array = s.toCharArray();
+        String result = "";
+        for (int i = array.length - 1; i >= 0; i--) {
+            result = result + array[i];
+        }
+        if (result.equalsIgnoreCase(s)) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+    }
+}
