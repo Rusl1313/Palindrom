693131148dd21578814f42fec414a6898c4cbd14
rusik <rusikss.ss13@gmail.com>
2023-02-14 16:05:40 +0300

    new

--- src/Palindrom.java
+++ src/Palindrom.java
@@ -7,7 +7,7 @@ public class Palindrom {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String symbol = scan.nextLine();
-        String s = symbol.replaceAll("[^a-zA-Z0-9]", "");
+        String s = symbol.replaceAll("[^a-zA-Zа-яА-я0-9]", "");
         char[] array = s.toCharArray();
         String result = "";
         for (int i = array.length - 1; i >= 0; i--) {
